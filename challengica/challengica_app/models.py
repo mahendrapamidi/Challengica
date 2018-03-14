cbf1a50800851533b2a1e3da2cc16fd3601fdbd7
krushiraj <krushiraj123@gmail.com>
2018-03-14 13:44:47 +0530

    present working commit

--- challengica/challengica_app/models.py
+++ challengica/challengica_app/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from datetime import datetime
+from django.utils import timezone
+from random import randint
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class SetNo(models.Model):
@@ -9,20 +13,30 @@ class SetNo(models.Model):
 
 class Question(models.Model):
     setno = models.ForeignKey(SetNo, on_delete=models.CASCADE)
+    ques_img = models.TextField(max_length=100)
     ques_text = models.TextField(max_length=3000)
     ans_text = models.TextField(max_length=1000)
 
     def __str__(self):
-        return "Set no: %s\nQuestion: %s\nAnswer: %s".format(self.setno, self.ques_text, self.ans_text)
+        return "Set no: %s\nQuestion: %s\nAnswer: %s" % (self.setno, self.ques_text, self.ans_text)
 
+def setendtime():
+    return timezone.now() + timezone.timedelta(minutes=35)
+    
 class Contestant(models.Model):
     username = models.CharField(max_length=20)
     email = models.CharField(max_length=100)
     contact = models.CharField(max_length=10)
     points = models.IntegerField(default=0)
-    starttime = models.DateTimeField()
-    endtime = models.DateTimeField()
+    year = models.IntegerField(
+        default=1,
+        validators=[MaxValueValidator(5), MinValueValidator(1)]
+    )
+    starttime = models.DateTimeField('start time', default = timezone.now)
+    endtime = models.DateTimeField('end time', default = setendtime)
     setno = models.ForeignKey(SetNo, on_delete=models.CASCADE, default=None)
 
     def __str__(self):
-        return "Username: {0}\n".format(self.username)
+        return "Username: %s\n" % (self.username)
+
+    
--- challengica/challengica_app/views.py
+++ challengica/challengica_app/views.py
@@ -1,7 +1,47 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import Http404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.defaults import page_not_found
+from django.utils import timezone
+from .models import Question,Contestant,SetNo
+from .forms import ContestantForm
+from random import randint 
 
 
 # Create your views here.
+def assignset(year):
+    year = int(year)
+    if year == 1:set_no = randint(1,2)
+    elif year==2:set_no = randint(3,4)
+    else: set_no = randint(5,6)
+    setobj = SetNo.objects.get(setno = set_no)
+    return setobj
+
 def index(request):
-    return HttpResponse("<strong>Hello!</strong>")
+    if request.method == "POST":
+        form = ContestantForm(request.POST)
+        if form.is_valid():
+            cont = form.save(commit=False)
+            cont.setno = assignset(request.POST.get('year',''))
+            cont.save()
+            return HttpResponseRedirect('set/'+str(cont.setno)+'/ques/welcome')
+    else:
+        form = ContestantForm()
+    return render(request, 'homepage.html', {'form':form})
+
+def welcome(request, set_no):
+    return render(request, 'rules.html')
+
+def ans_not_found(request):
+    return render(request, 'notfound.html', status=404)
+
+def generateques(request, set_no, ans):
+    try:
+        qs = Question.objects.get(setno = set_no, ans_text = ans)
+        q_no = qs.ques_img[3]
+        ques_text = qs.ques_text
+        meme_no = randint(1,5)
+    except Question.DoesNotExist:
+        meme_no = randint(1,9)
+        return render(request, 'notfound.html', {'meme_no':meme_no})
+    return render(request, 'questions.html', {'q_no':q_no, 'ques_text':ques_text, 'meme_no':meme_no})
