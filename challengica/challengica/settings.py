1f872ec323a61df18ec1a1ac1bcf93474208df2f
Krushi Raj Tula <krushiraj123@gmail.com>
2019-10-11 12:51:42 +0530

    Update settings.py
    Add static files URL and ROOT

--- challengica/challengica/settings.py
+++ challengica/challengica/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'challengica_app', 'static')
+
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'challengica_app','static'),
+    os.path.join(BASE_DIR, 'challengica_app', 'static'),
 )
